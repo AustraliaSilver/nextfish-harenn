c4b518662e659abd1d3045b2294c1124299b8600
AustraliaSilver <hunganhl642@gmail.com>
2026-04-14 20:25:19 +0700

    Fix chess import in generate_game_data function

--- generate_training_data.py
+++ generate_training_data.py
@@ -54,7 +54,6 @@ class HARENNDataGenerator:
 
     def generate_labels_from_trace(self, board) -> Optional[Dict]:
         try:
-            import chess
             import subprocess
             import chess
 
